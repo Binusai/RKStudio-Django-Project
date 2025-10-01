857cf2a5c3773a3b58fcc6bcb8669329cd940097
Poola Binu sai <149651174+Binusai@users.noreply.github.com>
2025-10-01 23:03:49 +0530

    Update settings.py

--- rk_studio/settings.py
+++ rk_studio/settings.py
@@ -88,11 +88,11 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Email configuration using environment variables
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'rkdigitalfashionstudio@gmail.com'
+EMAIL_HOST_PASSWORD = 'kjzlpxriycgljcls'
+DEFAULT_FROM_EMAIL = 'RK Studio rkdigitalfashionstudio@gmail.com'
